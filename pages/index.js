95ab800bea36d15abf5f4922ac0b80d3691428ab
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2021-05-09 01:36:11 +0530

    added all files

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { RichText } from "prismic-reactjs";
+import Prismic from "prismic-javascript";
+import { Client } from "../prismic-configuration";
+
+
+export async function getStaticProps()
+{
+  
+  const images = await Client().query(
+    Prismic.Predicates.at("document.type","images")
+  );
+    console.log("blog page",images.results)
+    
+    
+    
+  return {
+    props: {
+      images: images
+      
+    },
+  };
+}
+export default function Home({ images }) {
+  return (
+    <div >
+      <Head>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+      </Head>
+      <div className="">
+            <div className="logo text-center">
+              
+            </div>
+          </div>
+          <div >
+            <div className="parent">
+              <div className="photos">
+                {images.results.map((image, index) => (
+                  
+                
+                  (image.data.images_group.map((pic, index) => (
+                         <img src={pic.image.url} className="images" />
+                        
+                    )))
+                ))} 
+              </div>
+            </div>
+          </div>
+    
+    </div>
+  )
+}
