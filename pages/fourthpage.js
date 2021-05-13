57f30b66821e3f386bdde62476adb2ed0337c66d
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2021-05-14 01:22:41 +0530

    border issue solved in a unstable way

--- pages/fourthpage.js
+++ pages/fourthpage.js
@@ -10,18 +10,30 @@ export async function getStaticProps()
     Prismic.Predicates.at("document.type","images")
   );
     console.log("blog page",images.results)
-    
-    
+
+    console.log("blog page",images.results[0].data.images_group[0].image.url)
+    let image_links = [];
+    {images.results.map((image, index) => (
+                  
+                
+        (image.data.images_group.map((pic, index) => (
+            image_links.push(pic.image.url)
+        )))
+    ))} 
+    console.log("image url",image_links[1])
     
   return {
     props: {
-      images: images
+        image_links: image_links
+
       
     },
   };
 }
 
-function fourthpage({ images }) {
+function fourthpage({ image_links }) {
+    const [flag,setFlag] = useState(3)
+    const [width,setwidth] = useState(0)
     useEffect(() => {
        
         var grid = document.querySelector('.grid');
@@ -29,10 +41,27 @@ function fourthpage({ images }) {
         columns: '.grid-col',
         items: '.grid-item'
         });
-        
-
+        setwidth(window.screen.width)
     } );
+    useEffect(() => {   
+        if(window.screen.width<768)
+        {
+            setFlag(2)
+        }
+        else{
+            setFlag(3)
+        }
+    },[width]);
+    
+    let inputStyle = {
+        borderRight: "7px solid white",
+        borderBottom: "7px solid white"
+    };
+    let inputStyle2 = {
+        borderBottom: "7px solid white"
+    };
     return (
+
         <div>
         <Head>
             <link rel="stylesheet" href="assets/css/fourthpage.css" />
@@ -40,31 +69,38 @@ function fourthpage({ images }) {
             <script type="text/javascript" src="assets/js/colcade2.js"></script>
             
         </Head>
-        <div class="gallery-container">
+        <div className="gallery-container">
                 <h1>My Photographs</h1>
-
-                <div class="grid">
-                    <div class="grid-col grid-col--1">
-
-                    </div>
-                    <div class="grid-col grid-col--2">
+                {console.log(flag)}
+                <div className="grid">
+                    <div className="grid-col grid-col--1">
 
                     </div>
-                    <div class="grid-col grid-col--3">
+                    <div className="grid-col grid-col--2">
 
                     </div>
-                    <div class="grid-col grid-col--4">
+                    <div className="grid-col grid-col--3">
 
                     </div>
-                    {images.results.map((image, index) => (
-                  
-                
-                        (image.data.images_group.map((pic, index) => (
-                            <div class="grid-item" key={index}>
-                                <img src={pic.image.url} className="images" />
-                            </div> 
-                        )))
+                    <div className="grid-col grid-col--4">
+
+                    </div>
+                    {image_links.map((image_url, index) => (
+                        
+                        <div className="grid-item" key={index}>
+                            
+                            <img src={image_url} classNameName="images" style={ flag == 2 && index % 2 ? inputStyle2 : inputStyle} />
+                        </div> 
                     ))} 
+                    {/* {  for(i=0;image_links[i]!=null;i++){
+                        (<div className="grid-item" key={index}>
+                        <img src={image_url} classNameName="images" />
+                        </div>)
+                    }
+                        
+                        
+                     
+                    } */}
                 </div>
                 
             </div>
