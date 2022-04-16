03360febdbd02b10014d5d3107bee93889b548ad
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2022-04-16 19:52:24 +0530

    changed inputtag to yextare

--- pages/contact-me.js
+++ pages/contact-me.js
@@ -5,7 +5,7 @@ function contactMe() {
     return (
         <div >
             <Head>
-                <link href="/assets/css/contactme.css" rel="stylesheet"/>
+                <link href="/assets/css/contactme.css" rel="stylesheet" />
             </Head>
             <div >
                 <div className="contact-form-section">
@@ -16,37 +16,39 @@ function contactMe() {
                         <div className="first-name-last-name">
                             <div class="first-name">
                                 <label htmlFor="first">First Name</label>
-                                <input type="text" id="first" placeholder="enter your first name"/>
+                                <input type="text" id="first" placeholder="enter your first name" />
                             </div>
                             <div class="last-name">
                                 <label htmlFor="last">Last Name</label>
-                                <input type="text" id="last" placeholder="enter your last name"/>
+                                <input type="text" id="last" placeholder="enter your last name" />
                             </div>
                         </div>
                         <div class="email">
                             <label htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder="enter your address email"/>
+                            <input type="email" id="email" placeholder="enter your address email" />
                         </div>
                         <div className="phone-location">
                             <div class="phone">
                                 <label htmlFor="first">Phone</label>
-                                <input type="text" id="first" placeholder="enter your phone number"/>
+                                <input type="text" id="first" placeholder="enter your phone number" />
                             </div>
                             <div class="location">
                                 <label htmlFor="last">Location</label>
-                                <input type="text" id="last" placeholder="enter where are you from"/>
+                                <input type="text" id="last" placeholder="enter where are you from" />
                             </div>
                         </div>
                         <div class="email">
                             <label htmlFor="email">My favourite part: youselves!</label>
-                            <input type="email" id="email" placeholder="tell me about youselves"/>
+                            <textarea id="email" placeholder="tell me about youselves" />
                         </div>
                         <div class="email">
                             <label htmlFor="email">About Session</label>
-                            <input type="email" id="email" placeholder="tell me which session or what kinda photo service are you looking for?"/>
+                            <textarea id="email" placeholder="tell me which session or what kinda photo service are you looking for?" />
+                        </div>
+                        <div class="sub">
+                            <input type="submit" value="Send" />
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
